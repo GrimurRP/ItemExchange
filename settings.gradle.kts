c556d615591f5715345ea87b46dc3bfe8711848f
Aleksey Terzi <aleksey.terzi@gmail.com>
2022-09-22 18:03:06 -0600

    Remove CivGradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,10 @@
-rootProject.name = "itemexchange"
-
 pluginManagement {
 	repositories {
 		gradlePluginPortal()
 		maven("https://papermc.io/repo/repository/maven-public/")
-  		maven {
-			url = uri("https://maven.pkg.github.com/CivMC/CivGradle")
-			credentials {
-				username = System.getenv("GITHUB_ACTOR")
-				password = System.getenv("GITHUB_TOKEN")
-			}
-		}
 	}
 }
 
 include(":paper")
+
+project(":paper").name = rootProject.name
